78ceade4a6d476c31b80fd5ce4f0414368196afa
Joy Mohajon <92334273+mohajon33@users.noreply.github.com>
2023-01-21 01:21:56 +0600

    added uva 10104.cpp (Extended Euclidean Algorithm)

--- uva 10104.cpp
+++ uva 10104.cpp
@@ -1,18 +1,35 @@
 #include<bits/stdc++.h>
 
 using namespace std;
+using ll = long long;
 
-int gcd(int a, int b)
+//extended euclidean algorithm
+ll ex_gcd(ll a, ll b, ll& x, ll& y)
 {
-    // greatest common divisor
-    return (a%b==0)? b : (gcd(b, a%b));
+    if(b==0){
+        x = 1;
+        y = 0;
+        return a;
+    }
+    ll x1, y1;
+    ll gcd = ex_gcd(b, a%b, x1, y1);
+    x = y1;
+    y = x1 - y1*(a/b);
+    return gcd;
 }
+
 int main()
 {
-    int a, b;
-    while(cin >> a >> b){
+    ll a, b, x, y;
 
-        cout << gcd(a, b) << endl;
+    while(cin >> a >> b){
+        ll x, y;
+        ll gcd = a > b ? ex_gcd(a, b, x, y):ex_gcd(b, a, x, y);
+
+        if(a >= b)
+           cout << x << " " << y << " " << gcd << endl;
+        else 
+           cout << y << " " << x << " " << gcd << endl; 
     }
     return 0;
 }
